c2e7ac3d07b13a45e23cc0dcf16636df62e52d9b
matbarna12 <141824500+matbarna12@users.noreply.github.com>
2023-08-09 11:02:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
-streamlit.title('My Parents Healthy New Diner')
-
-streamlit.header('WeeWoo WeeWoo')
-
-streamlit.text('Zabbadoooooooo')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
