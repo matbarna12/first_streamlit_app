0a20a355bddecdfa78928a493d5cb9c5ed884d7f
matbarna12 <141824500+matbarna12@users.noreply.github.com>
2023-08-09 11:16:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,8 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Apple', 'Cherries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Apple', 'Cherries'])
+fruits_to_show = my_fruit_list.loc(fruits_selected)
 
-streamlit.dataframe(my_fruit_list)
+
+streamlit.dataframe(fruits_to_show)
